82720b10bacf3c97c018bf2ace0b25b63978ff71
Koshev <Koshev_prog@mail.ru>
2019-11-24 23:00:16 +0400

    create class Provider, interface Strategy

---
+++ src/main/java/model/Provider.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Provider {
+    Strategy strategy;
+
+    public Provider(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+}
