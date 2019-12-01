98ca38692164d1567ef70a61a504b49ff72ae26a
Kolll2 <39923388+Kolll2@users.noreply.github.com>
2019-12-01 14:49:42 +0400

    feat: added html template and few empty methods for to do later

--- src/main/java/aggr/view/HtmlView.java
+++ src/main/java/aggr/view/HtmlView.java
@@ -6,6 +6,10 @@ import aggr.vo.Vacancy;
 import java.util.List;
 
 public class HtmlView implements View {
+
+    private final String filePath = "./src/" +
+            this.getClass().getPackage().getName().replace('.', '/') +
+            "/vacancies.html";
     Controller controller;
 
     @Override
@@ -13,7 +17,9 @@ public class HtmlView implements View {
 //        for (Vacancy vac: vacancies) {
 //            System.out.println(vac.toString());
 //        }
-
+        String vacViewHTML = getUpdatedFileContent(vacancies);
+        updateFile(vacViewHTML);
+        System.out.println(filePath);
         System.out.println(vacancies.size());
     }
 
@@ -25,4 +31,19 @@ public class HtmlView implements View {
     public void userCitySelectEmulationMethod(){
         controller.onCitySelect("Odessa");
     }
+
+    /**
+     * To do later
+     * @param vacancies
+     * @return
+     */
+    private String getUpdatedFileContent(List<Vacancy> vacancies){
+        return null;
+    }
+
+    /**
+     * To do later
+     * @param fileName
+     */
+    private void updateFile(String fileName){}
 }
